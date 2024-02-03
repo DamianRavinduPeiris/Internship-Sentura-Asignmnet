74c2a37dcf0dc84cfc0592056e725bde360009d9
Damian Peiris. <drpeiris3@gmail.com>
2024-02-03 10:16:05 +0530

    Re-Updated and finalized User Update Component.

--- sentura/src/UpdateUser.tsx
+++ sentura/src/UpdateUser.tsx
@@ -8,14 +8,110 @@ import { useState } from "react";
 
 export default function UpdateUser() {
     const[userData,setUser]=useState(user);
+    const[id,setID]=useState('');
   return (
     <div>
         <h1>Update User.</h1>
         <div style={{ display: "flex", flexDirection: "column" }}>
-
         <input
           type="text"
-          placeholder="Which Field Do you want to update?"
+          placeholder="id"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setID(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="uid"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, uid: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, email: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="given_name"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, given_name: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="middle_name"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, middle_name: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="name"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, name: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="family_name"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, family_name: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="nickname"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, nickname: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="phone_number"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, phone_number: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="comment"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, comment: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="picture"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, picture: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="directory"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, directory: e.target.value });
+          }}
+        />
+        <input
+          type="text"
+          placeholder="meta_data"
           style={{ margin: "1rem" }}
           onChange={(e) => {
             setUser({ ...userData, meta_data: e.target.value });
@@ -23,7 +119,7 @@ export default function UpdateUser() {
         />
         <input
             type="text"
-            placeholder="Enter the value here."
+            placeholder="tags"
             style={{ margin: "1rem" }}
             onChange={(e) => {
                 
@@ -31,7 +127,14 @@ export default function UpdateUser() {
                 setUser({ ...userData, tags: tags } as typeof userData);
             }}
         />
-        
+        <input
+          type="text"
+          placeholder="is_suspended"
+          style={{ margin: "1rem" }}
+          onChange={(e) => {
+            setUser({ ...userData, is_suspended: e.target.value });
+          }}
+        />
         <button
           onClick={() => {
             let headers = {
@@ -40,7 +143,7 @@ export default function UpdateUser() {
             };
             axios
               .patch(
-                "https://7057f138d2224f01b4af1705865e57e7.weavy.io/api/users/"+userData.uid,
+                "https://7057f138d2224f01b4af1705865e57e7.weavy.io/api/users/"+id,
                 userData,
                 { headers: headers }
               )
